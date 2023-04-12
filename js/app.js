f2cdbab4f1bf2d76eecde67bab5294e0704a97a9
FadhlurRahman11 <nurfadhlurrahman@students.amikom.ac.id>
2023-04-12 14:46:51 +0700

    perubaham ke2

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(tombol).click(function(e){
+        $("div").hide('slow');
+    });
+})
+
+
+$('#satuinput').click(function (e) { 
+    $('.media').append("1");
+    
+});
+
+$('#duainput').click(function (e) { 
+    $('.media').append("2");
+    
+});
